0c1ec80abc0a1379dc8f1cb83fedb30146c18516
angeloalmanza <angeloalmanza26@gmail.com>
2024-12-02 13:55:25 +0100

    feat: implementazione rotta '/bacheca' e asset statici

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const posts = require('./data');
 const app = express();
 const port = 3000;
 
+// Asset Statico per le immagini
+app.use(express.static('public'));
+
+// Rotta '/'
 app.get('/', (req, res) => {
     res.json('Server del mio blog');
+})
+
+// Rotta '/bacheca' per i post
+app.get('/bacheca', (req,res) => {
+    const result = {
+        posts,
+        totale : posts.length
+    }
+
+    res.json(result);
 })
 
 app.listen(port, () => {
